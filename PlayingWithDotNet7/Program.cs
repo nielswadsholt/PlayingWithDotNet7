cd6f9186b7d2cb364908c840c0358ae4b4b8be43
Niels Wadsholt <nwadsholt@ymail.com>
2022-11-17 22:58:28 +0100

    More fun with strings

--- PlayingWithDotNet7/Program.cs
+++ PlayingWithDotNet7/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PlayingWithDotNet7;
+using System.Numerics;
 using System.Text;
+
+
+Console.WriteLine("========== Fun with numbers ==========");
 
 var ints = new[] { 1, 3, 6, 4, 2, 8 };
 var doubles = new[] { 1, 3, 6, 4.0, 2, 8 };
@@ -8,12 +12,12 @@ var decimals = new[] { 1, 3M, 6, 4, 2, 8 };
 byte[] bytes = { 0, 15, 42, 255 };
 var chars = new[] { 'a', 'b', 'c' };
 
-Console.WriteLine("========== Fun with numbers ==========");
 Console.WriteLine(ints.AddAll());
 Console.WriteLine(doubles.AddAll());
 Console.WriteLine(decimals.AddAll());
 Console.WriteLine(bytes.AddAll());
 Console.WriteLine(chars.AddAll());
+
 
 Console.WriteLine("\n========== Fun with strings ==========");
 
@@ -31,15 +35,25 @@ Console.WriteLine(""""
         </div>
     
     and you can't touch me!
+
     """");
 
 Console.WriteLine($""""
-    
     Or I can write
         Interpolated {Encoding.UTF8.GetString(u8)}
     if I please (which I do).
-    
+
     """");
+
+Console.WriteLine($$""""
+    But what if the text already contains curly braces?
+    Just add an extra dollar sign + pair of curly braces!
+    {
+        answer: {{42}}
+    }
+
+    """");
+
 
 Console.WriteLine("\n========== Fun with static members ==========");
 var myFirstNumber = new MyInt();
